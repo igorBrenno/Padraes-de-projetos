3fc86cc4ca9692bcf214cc765b3548aa19961371
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2025-11-06 18:58:55 +0000

    feat: criando composite e bridge

---
+++ estruturas/composite/main.py
@@ -0,0 +1,15 @@
+class graphic:
+
+    def move(x, y):
+        pass
+
+    def draw():
+        pass
+
+class dot(graphic):
+
+    def move(x, y):
+        pass
+    
+    def draw():
+        pass
